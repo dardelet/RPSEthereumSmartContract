8bac91a86df39358e97071ed1a65903fbbd7b76f
Guillaume Dardelet <guillaumegorp@gmail.com>
2018-05-04 20:43:28 -0400

    Change test for getWinner

--- test/rps.js
+++ test/rps.js
@@ -44,21 +44,24 @@ contract('RPS', accounts => {
   });
 
   it('getWinner method should compute the winner', async () => {
-    await rps.play('Rock', {from: playerOne});
-    await rps.play('Scissor', {from: playerTwo});
-    assert((await rps.getWinner.call() ) == 1); // playerOne wins
 
-    rps = await RPS.new({from: playerOne});
-    await rps.invitePlayerTwo(playerTwo, {from: playerOne});
-    await rps.play('Rock', {from: playerOne});
-    await rps.play('Rock', {from: playerTwo});
-    assert((await rps.getWinner.call()) == 0); // Draw
+    async function getWinnerOfGame(movePlayerOne, movePlayerTwo) {
+      rps = await RPS.new({from: playerOne});
+      await rps.invitePlayerTwo(playerTwo, {from: playerOne});
+      await rps.play(movePlayerOne, {from: playerOne});
+      await rps.play(movePlayerTwo, {from: playerTwo});
+      return rps.getWinner.call();
+    }
 
-    rps = await RPS.new({from: playerOne});
-    await rps.invitePlayerTwo(playerTwo, {from: playerOne});
-    await rps.play('Paper', {from: playerOne});
-    await rps.play('Scissor', {from: playerTwo});
-    assert((await rps.getWinner.call()) == 2); // playerTwo wins
-  })
+    assert((await getWinnerOfGame('Rock', 'Rock')).toNumber() === 0);
+    assert((await getWinnerOfGame('Rock', 'Paper')).toNumber() === 2);
+    assert((await getWinnerOfGame('Rock', 'Scissor')).toNumber() === 1);
+    assert((await getWinnerOfGame('Paper', 'Rock')).toNumber() === 1);
+    assert((await getWinnerOfGame('Paper', 'Paper')).toNumber() === 0);
+    assert((await getWinnerOfGame('Paper', 'Scissor')).toNumber() === 2);
+    assert((await getWinnerOfGame('Scissor', 'Rock')).toNumber() === 2);
+    assert((await getWinnerOfGame('Scissor', 'Paper')).toNumber() === 1);
+    assert((await getWinnerOfGame('Scissor', 'Scissor')).toNumber() === 0);
+  });
 });
 
