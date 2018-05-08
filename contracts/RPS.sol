af288cc7b73c17b02a8386ac2197f965604338fa
Guillaume Dardelet <guillaumegorp@gmail.com>
2018-05-08 00:17:01 -0500

    Add secured version

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.4.22;
 contract RPS {
   address public playerOne;
   address public playerTwo;
+  bool public secured;
+  bool private gameStarted;
   mapping (string => uint8) private moveToIndex;
   mapping (address => uint8) private playedMoves;
+  mapping (address => bytes32) private encryptedPlayedMoves;
 
   modifier onlyPlayerOne() {
     require(msg.sender == playerOne);
@@ -23,15 +26,22 @@ contract RPS {
     moveToIndex['Scissor'] = 3;
   }
 
+  function enableSecured() external onlyPlayerOne {
+    require(!gameStarted);
+    secured = true;
+  }
+
   function invitePlayerTwo(address _playerTwo) external onlyPlayerOne {
     playerTwo = _playerTwo;
   }
 
   function play(string move) external onlyPlayers {
+    require(!secured);
     uint8 index = moveToIndex[move];
     require(index > 0, 'Move was not found');
     require(playedMoves[msg.sender] == 0, 'A move was already played by the player');
     playedMoves[msg.sender] = index;
+    gameStarted = true;
   }
 
   // Returns 0 if draw, 1 if playerOne wins, or 2 if playerTwo wins
@@ -40,5 +50,33 @@ contract RPS {
     uint8 movePlayerTwo = playedMoves[playerTwo];
     require(movePlayerOne > 0 && movePlayerTwo > 0);
     return (3 + movePlayerOne - movePlayerTwo) % 3;
+  }
+
+  function playSecured(bytes32 encryptedMove) external onlyPlayers {
+    require(secured);
+    encryptedPlayedMoves[msg.sender] = encryptedMove;
+    gameStarted = true;
+  }
+
+  function submitSecret(string secret, string move) external onlyPlayers {
+    require(secured);
+    bytes32 encryptedMove = encryptedPlayedMoves[msg.sender];
+    require(keccak256(strConcat(secret, move)) == encryptedMove, 'secret need to match the encryptedMove sent earlier');
+    uint8 index = moveToIndex[move];
+    require(index > 0, 'Move was not found');
+    require(playedMoves[msg.sender] == 0, 'A move was already played by the player');
+    playedMoves[msg.sender] = index;
+  }
+
+  // From https://ethereum.stackexchange.com/questions/729/how-to-concatenate-strings-in-solidity#1203
+  function strConcat(string _a, string _b) internal returns (string) {
+    bytes memory _ba = bytes(_a);
+    bytes memory _bb = bytes(_b);
+    string memory ab = new string(_ba.length + _bb.length);
+    bytes memory bab = bytes(ab);
+    uint k = 0;
+    for (uint i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
+    for (i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+    return string(bab);
   }
 }
--- test/rps.js
+++ test/rps.js
@@ -17,7 +17,7 @@ contract('RPS', accounts => {
   });
 
   it('play method should store moves of the players', async () => {
-    // Those lines below test that the following smart contract call
+    // This try/catch will test that the RPS smart contract call
     // will fail with invalid parameters. If it does fail, the exception
     // will be caught and execution continues normally. If it does not
     // fail (as it should) the assert(false) will make the test itself fail
@@ -26,7 +26,7 @@ contract('RPS', accounts => {
       assert(false);
     } catch(e) {
       if (e.name == 'AssertionError')
-        assert(false, 'player should be able to play only valid move');
+        assert(false, 'player should be able to play only valid moves');
     }
 
     await rps.play('Rock', {from: playerOne});
@@ -70,5 +70,34 @@ contract('RPS', accounts => {
     assert((await getWinnerOfGame('Scissor', 'Paper')).toNumber() === PLAYER1);
     assert((await getWinnerOfGame('Scissor', 'Scissor')).toNumber() === DRAW);
   });
+
+  // Second part of the tests, for the secured version
+  it('enableSecured should set secured state variable to true', async () => {
+    rps = await RPS.new({from: playerOne});
+    await rps.invitePlayerTwo(playerTwo, {from: playerOne});
+
+    assert(!(await rps.secured.call()));
+    await rps.enableSecured({from: playerOne});
+    assert((await rps.secured.call()));
+  });
+
+  const secretPlayerOne = 'c6d9a6ccbe3a39396869dccce3a288649a2fc486';
+  const secretPlayerTwo = '042e4f12cdcef974dafa7338ed2fe8506a8a21e0';
+
+  it('playedSecured should store encryptedMoves of players', async () => {
+    // If an error is thrown by the contract, the test will fail
+    await rps.playSecured(web3.sha3(secretPlayerOne + 'Rock'), {from: playerOne});
+    await rps.playSecured(web3.sha3(secretPlayerTwo + 'Paper'), {from: playerTwo});
+  });
+
+  it('submitSecret should verify that the secret matches the move sent earlier', async () => {
+    await rps.submitSecret(secretPlayerOne, 'Rock', {from: playerOne});
+    await rps.submitSecret(secretPlayerTwo, 'Paper', {from: playerTwo});
+  });
+
+  it('getWinner should also work for the secured version, once secrets have been submitted', async () => {
+    const winner = await rps.getWinner.call()
+    assert(winner.toNumber() === 2);
+  })
 });
 
